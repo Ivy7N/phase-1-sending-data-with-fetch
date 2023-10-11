70839a7f31b2222fe62559d57a426d7ae8b110b6
Ivy7N <ivy.ngatia@student.moringaschool.com>
2023-10-12 01:47:42 +0300

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData(name, email) {
+    const userData = {
+      name: name,
+      email: email
+    };
+  
+    const configuration = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(userData)
+    };
+  
+    return fetch("http://localhost:3000/users", configuration)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        document.body.innerHTML = data.id;
+      })
+      .catch(function(error) {
+        document.body.innerHTML = error.message;
+      });
+  }
+  
